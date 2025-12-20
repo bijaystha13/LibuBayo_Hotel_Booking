7ab1e0e54f936fae5ce60d43b412c717774acacf
BIJAY SHRESHTHA <shresthabzay95@gmail.com>
2025-12-20 09:30:57 -0500

    Custom Form Hook Added

---
+++ app/shared/hooks/formHook.ts
@@ -0,0 +1,148 @@
+// import { useCallback, useReducer } from "react";
+
+// type FormInput = {
+//   value: string;
+//   isValid: boolean;
+// };
+
+// type FormState = {
+//   inputs: {
+//     [key: string]: FormInput;
+//   };
+//   isValid: boolean;
+// };
+
+// type FormAction =
+//   | {
+//       type: "INPUT_CHANGE";
+//       inputId: string;
+//       value: string;
+//       isValid: boolean;
+//     }
+//   | {
+//       type: "SET_MODE";
+//       inputs: { [key: string]: FormInput };
+//     };
+
+// function formReducer(state: FormState, action: FormAction): FormState {
+//   switch (action.type) {
+//     case "INPUT_CHANGE":
+//       let formIsValid = true;
+//       for (const inputId in state.inputs) {
+//         if (!state.inputs[inputId]) continue;
+//         if (inputId === action.inputId) {
+//           formIsValid = formIsValid && action.isValid;
+//         } else {
+//           formIsValid = formIsValid && state.inputs[inputId].isValid;
+//         }
+//       }
+//       return {
+//         ...state,
+//         inputs: {
+//           ...state.inputs,
+//           [action.inputId]: { value: action.value, isValid: action.isValid },
+//         },
+//         isValid: formIsValid,
+//       };
+//     case "SET_MODE":
+//       return {
+//         ...state,
+//         inputs: action.inputs,
+//         isValid: false,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export function useForm(intialInputs, initialFormValidiry) {
+//   const [formState, dispatch] = useReducer(formReducer, {
+//     inputs: intialInputs,
+//     isValid: initialFormValidiry,
+//   });
+
+//   const inputHandler = useCallback(
+//     (id: string, value: string, isValid: boolean) => {
+//       dispatch({ type: "INPUT_CHANGE", value: value, isValid, inputId: id });
+//     },
+//     []
+//   );
+
+//   return [formState, inputHandler];
+// }
+
+import { useCallback, useReducer } from "react";
+
+type FormInput = {
+  value: string;
+  isValid: boolean;
+};
+
+type FormState = {
+  inputs: {
+    [key: string]: FormInput;
+  };
+  isValid: boolean;
+};
+
+type FormAction =
+  | {
+      type: "INPUT_CHANGE";
+      inputId: string;
+      value: string;
+      isValid: boolean;
+    }
+  | {
+      type: "SET_MODE";
+      inputs: { [key: string]: FormInput };
+    };
+
+function formReducer(state: FormState, action: FormAction): FormState {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (!state.inputs[inputId]) continue;
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    case "SET_MODE":
+      return {
+        ...state,
+        inputs: action.inputs,
+        isValid: false,
+      };
+    default:
+      return state;
+  }
+}
+
+export function useForm(
+  initialInputs: { [key: string]: FormInput },
+  initialFormValidity: boolean
+) {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValidity,
+  });
+
+  const inputHandler = useCallback(
+    (id: string, value: string, isValid: boolean) => {
+      dispatch({ type: "INPUT_CHANGE", value, isValid, inputId: id });
+    },
+    []
+  );
+
+  return [formState, inputHandler, dispatch] as const;
+}
